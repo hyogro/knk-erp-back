b120410138c9ae09527faacb4cba6b569ac870d6
shlee <dltkdgns3435@naver.com>
2021-06-03 14:00:08 +0900

    shlee_20210603_1341_create 인터셉터 생성

--- src/main/java/knk/erp/api/shlee/config/WebConfiguration.java
+++ src/main/java/knk/erp/api/shlee/config/WebConfiguration.java
@@ -1,7 +1,9 @@
 package knk.erp.api.shlee.config;
 
+import knk.erp.api.shlee.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,5 +11,10 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
     }
 }
--- src/main/java/knk/erp/api/shlee/interceptor/LogInterceptor.java
+++ src/main/java/knk/erp/api/shlee/interceptor/LogInterceptor.java
@@ -1,5 +1,7 @@
 package knk.erp.api.shlee.interceptor;
 
+import knk.erp.api.shlee.common.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +20,16 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("=start-request=");
+        String token = request.getHeader("token");
+        logger.info("==================== START ====================");
+        logger.info("요청 토큰: {}", token.substring(7));
         logger.info("request_host: {}:{}\trequest_url: {}", request.getRemoteHost(), request.getRemotePort(), request.getRemoteAddr());
         return super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info("=end-request=");
+        logger.info("===================== END =====================");
         super.postHandle(request, response, handler, modelAndView);
     }
 }
