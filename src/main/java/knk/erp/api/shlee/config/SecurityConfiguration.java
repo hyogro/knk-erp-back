7d1d132daf74803f7535dd9406d36e882f17119d
shlee <dltkdgns3435@naver.com>
2021-06-08 15:26:46 +0900

    shlee_20210608_1526 update SecurityConfiguration에 cors 세팅 추가

--- src/main/java/knk/erp/api/shlee/config/SecurityConfiguration.java
+++ src/main/java/knk/erp/api/shlee/config/SecurityConfiguration.java
@@ -59,11 +59,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
 
-                //2021-06-08 15:24 이상훈 추가 Cors 세팅
-                .authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .and()
-
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .apply(new knk.erp.api.shlee.config.JwtSecurityConfig(tokenProvider));
     }
