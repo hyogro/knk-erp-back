3ceb160fbc1652f6ab3ab8a629dac9f1338ded95
shlee <dltkdgns3435@naver.com>
2021-06-11 11:15:27 +0900

    shlee_20210611_1115 update 공통유틸 이름변경

--- src/main/java/knk/erp/api/shlee/common/util/CommonUtil.java
+++ src/main/java/knk/erp/api/shlee/common/util/CommonUtil.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class CommonUtil {
 
     //레벨 3 이상인지 여부 체크
-    public int checkMaster(Authentication authentication) {
+    public int checkLevel(Authentication authentication) {
         String lvl = authentication.getAuthorities().toString().replace("[ROLE_", "").replace("]", "");
         return lvl.equals("LVL2") ? 2 :lvl.equals("LVL3") ? 3 : lvl.equals("LVL4") ? 4 : lvl.equals("ADMIN") ? 5 : 1;
     }
