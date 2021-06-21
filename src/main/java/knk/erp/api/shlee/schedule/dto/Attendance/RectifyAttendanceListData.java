2c5b06e2ee6bfa08960024a1c1ea081271dea66e
shlee <dltkdgns3435@naver.com>
2021-06-21 09:59:57 +0900

    shlee_20210621_0959 update 정정요청 조회 리스폰스 추가

--- src/main/java/knk/erp/api/shlee/schedule/dto/Attendance/RectifyAttendanceListData.java
+++ src/main/java/knk/erp/api/shlee/schedule/dto/Attendance/RectifyAttendanceListData.java
@@ -15,11 +15,13 @@ import java.time.LocalTime;
 public class RectifyAttendanceListData {
 
     private Long id;
+    private String memberName;
     private LocalDate attendanceDate;
     private LocalDateTime createDate;
 
     public RectifyAttendanceListData(RectifyAttendance rectifyAttendance){
         this.id = rectifyAttendance.getId();
+        this.memberName = rectifyAttendance.getAuthor().getMemberName();
         this.attendanceDate = rectifyAttendance.getAttendanceDate();
         this.createDate = rectifyAttendance.getCreateDate();
     }
