9590f54f5b423b2554796db3776301f0608ffada
sh-mac <dltkdgns3435@naver.com>
2022-03-22 11:34:38 +0900

    휴가 오류검토 로그 추가

--- src/main/java/knk/erp/api/shlee/domain/schedule/util/VacationUtil.java
+++ src/main/java/knk/erp/api/shlee/domain/schedule/util/VacationUtil.java
@@ -4,12 +4,14 @@ import knk.erp.api.shlee.domain.schedule.dto.Vacation.AddVacationListData;
 import knk.erp.api.shlee.domain.schedule.dto.Vacation.VacationListData;
 import knk.erp.api.shlee.domain.schedule.entity.AddVacation;
 import knk.erp.api.shlee.domain.schedule.entity.Vacation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 public class VacationUtil {
 
     public List<Object> AddVacationListToDTO(List<AddVacation> addVacationList){
@@ -23,6 +25,7 @@ public class VacationUtil {
     public List<Object> VacationListToDTO(List<Vacation> vacationList){
         List<Object> vacationDTOList = new ArrayList<>();
         for(Vacation vacation : vacationList){
+            log.info("vac_id: {}", vacation.getId());
             vacationDTOList.add(new VacationListData(vacation));
         }
         return vacationDTOList;
